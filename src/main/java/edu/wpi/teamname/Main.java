cc6da0c2b74b22291a11fa552874fb3e94e19755
Lorenzo Lopez <lplopez@wpi.edu>
2020-03-26 13:32:13 -0400

    Added Lorenzo ffrom sysout

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,9 +1,23 @@
 package edu.wpi.teamname;
 
 public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
 
   public static void main(String[] args) {
     App.launch(App.class, args);
     System.out.println("Lorenzo Lopez");
+
+    System.out.println("Ben Robinson");
+
+    System.out.println("Denver Blake");
+    System.out.println("Ryan C");
+    System.out.println("Megan Throlson")
+  }
+
+  public static void printDenverBlake() {
+    System.out.println("Denver Blake");
+
   }
 }
