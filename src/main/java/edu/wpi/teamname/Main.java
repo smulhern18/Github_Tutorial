4f5d0edb828f4e6be53b9b3c63ea2b18390c9cf5
MeganThrolson <42652883+MeganThrolson@users.noreply.github.com>
2020-03-26 13:10:12 -0400

    fixed small change

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,6 +3,7 @@ package edu.wpi.teamname;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello");
+  }
 
   public static void main(String[] args) {
     App.launch(App.class, args);
