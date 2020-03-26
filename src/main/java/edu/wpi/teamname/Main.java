8512dbad1b897f8abf0edb0745bda5d9718ab6aa
rfcarnemolla <rfcarnemolla@wpi.edu>
2020-03-26 13:04:16 -0400

    added ryan name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,10 +1,10 @@
 package edu.wpi.teamname;
 
 public class Main {
-  System.out.println("Ryan C");
   public static void main(String[] args) {
     App.launch(App.class, args);
     System.out.println("Denver Blake");
+    System.out.println("Ryan C");
   }
 
   public static void printDenverBlake() {
